3d66c57920851279309704e5ef53ed5f4df1cef8
yugz <yugandhar.patil@learner.manipal.edu>
2024-04-30 10:38:29 +0530

    part 3 complte

--- cmd/app/block.go
+++ cmd/app/block.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
 type Block struct {
-	Timestamp                 int64  // timestamp of when block is created
-	Data, PrevBlockHash, Hash []byte // data contains valuable information, prevBlockHash stored prev block hash, Hash is the hash of this block
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10)) // FormatInt converts Timestamp to decimal number as string
-	// []byte() converts the string to a byte slice like [104 101 108 108 111]
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{}) // all properties other than current block hash concatenated
-	hash := sha256.Sum256(headers)                                                // hash of current block generated
-
-	b.Hash = hash[:] // same as b.Hash = hash
+	Timestamp                 int64
+	Data, PrevBlockHash, Hash []byte
+	Nonce                     int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}} // newly malloced block
-	block.SetHash()
-	return &block // new block address returned (pointer to new block)
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+	return block
 }
 
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
+
+// 	b.Hash = hash[:]
+// }
+
 func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{}) // creates a Genesis block using malloc
+	return NewBlock("Genesis Block", []byte{})
 }
--- cmd/app/blockchain.go
+++ cmd/app/blockchain.go
@@ -1,16 +1,15 @@
 package main
 
 type Blockchain struct {
-	blocks []*Block // a slice with each element of "pointer to block" data type
+	blocks []*Block
 }
 
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1] // last block (before inserted of new block)
-	newBlock := NewBlock(data, prevBlock.Hash)
+	prevBlock := bc.blocks[len(bc.blocks)-1]   // last block address
+	newBlock := NewBlock(data, prevBlock.Hash) // automatically deferences Hash from memory location even if prevBlock is a pointer and not the block itself
 	bc.blocks = append(bc.blocks, newBlock)
 }
 
 func NewBlockchain() *Blockchain {
-	blockchain := Blockchain{[]*Block{NewGenesisBlock()}}
-	return &blockchain
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
